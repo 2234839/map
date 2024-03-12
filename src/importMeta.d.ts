de77c9d8b7bd8e27048395343ddb38a6d7f1e7f7
llej <admin@shenzilong.cn>
2024-03-12 15:37:46 +0000

    解耦默认坐标

--- src/importMeta.d.ts
+++ src/importMeta.d.ts
@@ -1,4 +1,8 @@
 interface ImportMetaEnv {
   /* 瓦片地图接口 */
   VITE_MAP_URL: string;
+  // 打开地图默认展示坐标的经度
+  VITE_DEFAULT_LNG:string
+  // 打开地图默认展示坐标的纬度
+  VITE_DEFAULT_LAT:string
 }
--- src/pages/map/map.tsx
+++ src/pages/map/map.tsx
@@ -4,18 +4,36 @@ import { signal } from "@preact/signals";
 import "leaflet/dist/leaflet.css";
 export function Map() {
   console.log(import.meta.env);
-  
-  const map = signal<null | L.Map>(null);
+
+  const map$ = signal<null | L.Map>(null);
   useEffect(() => {
-    map.value = L.map("map").setView([51.505, -0.09], 13);
+    const map = L.map("map").setView(
+      [
+        Number(import.meta.env.VITE_DEFAULT_LAT),
+        Number(import.meta.env.VITE_DEFAULT_LNG),
+      ],
+      15
+    );
+    map$.value = map;
     L.tileLayer(import.meta.env.VITE_MAP_URL, {
       maxZoom: 19,
       attribution:
         '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    }).addTo(map.value);
+    }).addTo(map);
+    var polygon = L.polygon([
+      [51.509, -0.08],
+      [51.503, -0.06],
+      [51.51, -0.047],
+    ]).addTo(map);
+
+    function onMapClick(e) {
+      console.log(e);
+    }
+
+    map.on("click", onMapClick);
     return () => {
-      if (map.value) {
-        map.value.remove();
+      if (map$.value) {
+        map$.value.remove();
       }
     };
   }, []);
